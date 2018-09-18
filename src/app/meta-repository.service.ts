de7dbcb6964d77a7afdf424ee9ec718b65f706c0
babich-a <alexey.babych@gmail.com>
2018-09-18 13:20:39 +0300

    fix after merge

---
+++ src/app/meta-repository.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+import * as MetadataLoader from 'devextreme-themebuilder/modules/metadata-loader';
+import * as MetadataRepository from 'devextreme-themebuilder/modules/metadata-repository';
+import * as themes from 'devextreme-themebuilder/modules/themes';
+
+
+
+@Injectable()
+export class MetadataRepositoryService {
+
+    private metadataRepository: MetadataRepository;
+    private metadataPromise: Promise<any>;
+
+    constructor() {
+        this.metadataRepository = new MetadataRepository(new MetadataLoader());
+        this.metadataPromise = this.metadataRepository.init(themes);
+    }
+
+    getData(theme: any) {
+        return this.metadataPromise.then(() => {
+            return this.metadataRepository.getData(theme);
+        });
+    }
+
+    updateData(data: Array<any>, theme: any) {
+        return this.metadataPromise.then(() => {
+            return this.metadataRepository.updateData(data, theme);
+        });
+    }
+
+    getDataItemByKey(key: string, theme: any) {
+        return this.metadataPromise.then(() => {
+            return this.metadataRepository.getDataItemByKey(key, theme);
+        });
+    }
+
+    getModifiedData(theme: any) {
+        return this.metadataPromise.then(() => {
+            const result = [];
+            const themeData = this.metadataRepository.getData(theme);
+            for(const t in themeData) {
+                if(themeData.hasOwnProperty(t)) {
+                    const groups = themeData[t];
+                    groups.forEach(item => {
+                        if(item.IsModified) {
+                            result.push({ key: item.Key, value: item.Value });
+                        }
+                    });
+                }
+            }
+            return result;
+        });
+    }
+}
