4993be4835cd8d535a0e1bf30c7b5608a4f1e287
Alexey Babich <babich-a@users.noreply.github.com>
2018-09-25 14:42:27 +0300

    Use sass.js as sassCompiler (#10)

--- src/app/builder.service.ts
+++ src/app/builder.service.ts
@@ -4,6 +4,7 @@ import * as lessCompiler from 'less/lib/less-browser';
 import * as builder from 'devextreme-themebuilder';
 import { ExportedItem } from './types/exported-item';
 import { Theme } from './types/theme';
+import * as Sass from 'sass.js/dist/sass.js';
 
 
 @Injectable()
@@ -17,9 +18,27 @@ export class BuilderService {
         };
     }
 
+    scssCompiler: any = {
+        render: (scss) => {
+            Sass.setWorkerUrl('sass/sass.worker.js');
+            const sass = new Sass();
+            return new Promise((resolve, reject) => {
+                sass.compile(scss, result => {
+                    if(result.status === 0) {
+                        resolve(result.text);
+                    } else {
+                        reject(result);
+                    }
+                });
+            });
+        }
+    };
+
+
     buildTheme(theme: Theme, makeSwatch: boolean, outColorScheme: string, modifiedData: Array<ExportedItem>) {
         return builder.buildTheme({
             lessCompiler: lessCompiler(window, {}),
+            sassCompiler: this.scssCompiler,
             makeSwatch: makeSwatch,
             outputColorScheme: outColorScheme,
             reader: this.loadLess,
