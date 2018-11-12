005e32876c4b8d759fa2f780066de2f9a2af8498
Alexey Babich <alexey.babych@gmail.com>
2018-11-12 11:49:24 +0300

    Remove advanced layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { AppLayoutComponent } from './layouts/app-layout/app-layout.component';
 import { PreviewLayoutComponent } from './layouts/preview-layout/preview-layout.component';
-import { AdvancedLayoutComponent } from './layouts/advanced-layout/advanced-layout.component';
 
 import { IndexComponent } from './index/index.component';
 import { MasterComponent } from './master/master.component';
@@ -23,16 +22,10 @@ const routes: Routes = [
             { path: 'import', component: IndexComponent, data: { routeId: 2 } },
             { path: 'import/bootstrap', component: ImportBootstrapComponent, data: { routeId: 3 } },
             { path: 'import/meta', component: ImportMetaComponent, data: { routeId: 3 } },
-            { path: 'master/:theme/:color-scheme', component: MasterComponent, data: { routeId: 3 } }
-        ]
-    },
-    {
-        path: '',
-        component: AdvancedLayoutComponent,
-        children: [
+            { path: 'master/:theme/:color-scheme', component: MasterComponent, data: { routeId: 3 } },
             { path: 'advanced', redirectTo: '/advanced/generic/light/base.common', pathMatch: 'full' },
             { path: 'advanced/:theme/:color-scheme', redirectTo: 'advanced/:theme/:color-scheme/base.common', pathMatch: 'full' },
-            { path: 'advanced/:theme/:color-scheme/:widget', component: AdvancedComponent },
+            { path: 'advanced/:theme/:color-scheme/:widget', component: AdvancedComponent, data: { routeId: 4 } },
         ]
     },
     {
