e3878bd17acf45080488668a4c6f99ab842266d7
Victor Shchelkunov <viterobk@users.noreply.github.com>
2019-05-21 10:05:07 +0300

    Add "help" button

---
+++ src/app/layouts/header/help-button/help-button.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { Router, ActivationEnd } from '@angular/router';
+
+import { filter } from 'rxjs/operators';
+
+@Component({
+    selector: 'app-help-button',
+    templateUrl: './help-button.component.html',
+    styleUrls: ['./help-button.component.css']
+})
+export class HelpButtonComponent {
+    readonly baseUrl: string = 'https://js.devexpress.com/Documentation/Guide/Themes_and_Styles/ThemeBuilder/';
+    public $docUrl: string;
+
+    constructor(private router: Router) {
+        this.router.events.subscribe((event) => {
+            if(event instanceof ActivationEnd) {
+                if(!(event.snapshot.data.docHash === undefined)) {
+                    this.$docUrl = `${this.baseUrl}${event.snapshot.data.docHash}`;
+                }
+            }
+        });
+    }
+}
