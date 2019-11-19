9c685b9b13cb9ecdcc3d9c3adce0eb717549150e
BingoRUS <bingofirst@gmail.com>
2019-11-19 14:09:14 +0300

    fix lint errors

--- src/app/preview/buttons/buttons.component.ts
+++ src/app/preview/buttons/buttons.component.ts
@@ -8,41 +8,41 @@ import { BehaviorSubject } from 'rxjs';
 })
 
 export class ButtonsComponent {
+    constructor() {
+        this.baseOptions = [
+            { text: 'Normal', type: 'normal', fontStyles: this.getFontStyles('normal') },
+            { text: 'Success', type: 'success', fontStyles: this.getFontStyles('success') },
+            { text: 'Default', type: 'default', fontStyles: this.getFontStyles('default') },
+            { text: 'Danger', type: 'danger', fontStyles: this.getFontStyles('danger') }
+        ];
+    }
+
     widgetGroup = 'buttons';
     isExpanded = new BehaviorSubject<boolean>(false);
-
-    baseOptions: any[] = [
-        { text: 'Normal', type: 'normal', fontStyles: this.getFontStyles('normal') },
-        { text: 'Success', type: 'success', fontStyles: this.getFontStyles('success') },
-        { text: 'Default', type: 'default', fontStyles: this.getFontStyles('default') },
-        { text: 'Danger', type: 'danger', fontStyles: this.getFontStyles('danger') }
-    ];
-
+    baseOptions: any[];
     buttonModes: string[] = ['contained', 'outlined', 'text'];
     buttonGroupModes: string[] = ['contained', 'outlined', 'text'];
 
-    getFontStyles(type: string): FontStyle[] {
-        return [
-            {
-                icon: "bold",
-                style: "bold",
-                hint: "Bold",
-                type: type
-            },
-            {
-                icon: "italic",
-                style: "italic",
-                hint: "Italic",
-                type: type
-            },
-            {
-                icon: "underline",
-                style: "underline",
-                hint: "Underlined",
-                type: type
-            }
-        ];
-    }
+    getFontStyles = (type: string) => [
+        {
+            icon: 'bold',
+            style: 'bold',
+            hint: 'Bold',
+            type: type
+        },
+        {
+            icon: 'italic',
+            style: 'italic',
+            hint: 'Italic',
+            type: type
+        },
+        {
+            icon: 'underline',
+            style: 'underline',
+            hint: 'Underlined',
+            type: type
+        }
+    ];
 }
 
 export class FontStyle {
