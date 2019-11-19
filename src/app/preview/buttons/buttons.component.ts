dd35cf343bd94294bffa3a5e775b265ff4eb6dae
Sergey Zvyagin <bingofirst@gmail.com>
2019-11-19 14:12:10 +0300

    Add ButtonGroup and its constants (#119)

--- src/app/preview/buttons/buttons.component.ts
+++ src/app/preview/buttons/buttons.component.ts
@@ -6,16 +6,48 @@ import { BehaviorSubject } from 'rxjs';
     templateUrl: './buttons.component.html',
     styleUrls: ['./buttons.component.css']
 })
+
 export class ButtonsComponent {
+    constructor() {
+        this.baseOptions = [
+            { text: 'Normal', type: 'normal', fontStyles: this.getFontStyles('normal') },
+            { text: 'Success', type: 'success', fontStyles: this.getFontStyles('success') },
+            { text: 'Default', type: 'default', fontStyles: this.getFontStyles('default') },
+            { text: 'Danger', type: 'danger', fontStyles: this.getFontStyles('danger') }
+        ];
+    }
+
     widgetGroup = 'buttons';
     isExpanded = new BehaviorSubject<boolean>(false);
+    baseOptions: any[];
+    buttonModes: string[] = ['contained', 'outlined', 'text'];
+    buttonGroupModes: string[] = ['contained', 'outlined', 'text'];
 
-    baseOptions: any[] = [
-        { text: 'Normal', type: 'normal' },
-        { text: 'Success', type: 'success' },
-        { text: 'Default', type: 'default' },
-        { text: 'Danger', type: 'danger' }
+    getFontStyles = (type: string) => [
+        {
+            icon: 'bold',
+            style: 'bold',
+            hint: 'Bold',
+            type: type
+        },
+        {
+            icon: 'italic',
+            style: 'italic',
+            hint: 'Italic',
+            type: type
+        },
+        {
+            icon: 'underline',
+            style: 'underline',
+            hint: 'Underlined',
+            type: type
+        }
     ];
+}
 
-    modes: string[] = ['contained', 'outlined', 'text'];
+export class FontStyle {
+    icon: string;
+    style: string;
+    hint: string;
+    type: string;
 }
