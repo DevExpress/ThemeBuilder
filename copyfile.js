403923b6078862276ace0958cf2a5e2ce12048e9
babich-a <alexey.babych@gmail.com>
2021-01-26 10:15:29 +0300

    copy 404 file

---
+++ copyfile.js
@@ -0,0 +1,4 @@
+'use strict';
+
+const { copyFileSync } = require('fs'); // eslint-disable-line
+copyFileSync('dist/devextreme-themebuilder-app/index.html', 'dist/devextreme-themebuilder-app/404.html');
