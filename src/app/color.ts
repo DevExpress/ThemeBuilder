7db6cfa2bc98d6f14392fe92a242579b3b775d9c
OlegKipchatov <kipchatovbrend@yandex.ru>
2022-08-02 20:49:33 +0400

    Fix lint

--- src/app/color.ts
+++ src/app/color.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-magic-numbers */
+
 export const hexToRgba = (hex: string): string => {
     if(!hex || !hex.startsWith('#')) return hex;
 
@@ -7,6 +9,6 @@ export const hexToRgba = (hex: string): string => {
     const green = parseInt(hex.slice(2, 4), 16);
     const blue = parseInt(hex.slice(4, 6), 16);
     const alpha = (parseInt(hex.slice(6, 8) || 'ff', 16) / 255).toFixed(2);
-    
+
     return `rgba(${red}, ${green}, ${blue}, ${alpha})`;
 };
