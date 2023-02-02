b00f3d8b154f2fbaf30a3d8d10cf591ceb378cfe
OlegKipchatov <kipchatovbrend@yandex.ru>
2023-02-02 10:43:12 +0400

    Remove custom 4/8 digit hex code

--- src/app/left-menu/editor/editor.component.ts
+++ src/app/left-menu/editor/editor.component.ts
@@ -5,31 +5,19 @@ import { SafeHtml } from '@angular/platform-browser';
 import { MetadataRepositoryService } from '../../meta-repository.service';
 import { NamesService } from '../../names.service';
 import { MetaItem } from '../../types/meta-item';
-import { hexToRgba } from '../../color';
 
 @Component({
     selector: 'app-editor',
     templateUrl: './editor.component.html',
     styleUrls: ['./editor.component.css']
 })
-export class EditorComponent implements OnInit {
+export class EditorComponent {
     @Input('item') item: MetaItem;
 
     @Input() searchText = '';
 
-    header: SafeHtml;
-
     constructor(private names: NamesService,
         private metaRepository: MetadataRepositoryService) { }
-
-    ngOnInit(): void {
-        this.header = this.highlight(this.item.Name);
-        if(this.item.Value) {
-            if(this.item.Type === 'color') {
-                this.item.Value = hexToRgba(this.item.Value);
-            }
-        }
-    }
 
     highlight(text: string): SafeHtml {
         return this.names.getHighlightedForLeftMenuName(text, this.searchText);
