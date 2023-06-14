65cda939749641f1f54093629e06cb3f619b4644
Oleg Kipchatov <35765711+OlegKipchatov@users.noreply.github.com>
2023-06-14 14:25:38 +0400

    Prepare ThemeBuilder to 23.1 (#174)

--- src/app/left-menu/editor/editor.component.ts
+++ src/app/left-menu/editor/editor.component.ts
@@ -1,35 +1,23 @@
 /* eslint-disable @typescript-eslint/no-magic-numbers */
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { SafeHtml } from '@angular/platform-browser';
 
 import { MetadataRepositoryService } from '../../meta-repository.service';
 import { NamesService } from '../../names.service';
 import { MetaItem } from '../../types/meta-item';
-import { hexToRgba } from '../../color';
 
 @Component({
     selector: 'app-editor',
     templateUrl: './editor.component.html',
     styleUrls: ['./editor.component.css']
 })
-export class EditorComponent implements OnInit {
+export class EditorComponent {
     @Input('item') item: MetaItem;
 
     @Input() searchText = '';
 
-    header: SafeHtml;
-
     constructor(private names: NamesService,
         private metaRepository: MetadataRepositoryService) { }
-
-    ngOnInit(): void {
-        this.header = this.highlight(this.item.Name);
-        if(this.item.Value) {
-            if(this.item.Type === 'color') {
-                this.item.Value = hexToRgba(this.item.Value);
-            }
-        }
-    }
 
     highlight(text: string): SafeHtml {
         return this.names.getHighlightedForLeftMenuName(text, this.searchText);
