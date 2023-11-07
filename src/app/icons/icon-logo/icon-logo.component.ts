e3ee47f1e3d7ef1a30b7930c9709ceda64ddb37d
Aliullov Vlad <91639107+GoodDayForSurf@users.noreply.github.com>
2023-11-07 13:50:01 +0400

    Redesign header, footer, favicon (#196) (#202)
    * redesign
    +header logo
    +footer logo
    +favicon
    
    (cherry picked from commit db6895106536d4985fa47e7e27c868dc41588c24)

--- src/app/icons/icon-logo/icon-logo.component.ts
+++ src/app/icons/icon-logo/icon-logo.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-icon-logo',
-    templateUrl: './icon-logo.component.html'
+    templateUrl: './icon-logo.component.html',
+    styleUrls: ['./icon-logo.component.css']
 })
 export class IconLogoComponent { }
