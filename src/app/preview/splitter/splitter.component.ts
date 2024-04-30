e3a4a83e64a374982a99e701b9afc681a5449ecd
dxvladslavvolkov <vladislav.volkov@devexpress.com>
2024-04-30 17:07:08 +0400

    Fix lint

--- src/app/preview/splitter/splitter.component.ts
+++ src/app/preview/splitter/splitter.component.ts
@@ -4,7 +4,7 @@ import { BehaviorSubject, Subscription } from 'rxjs';
 interface PaneContentTemplate {
     name: string;
     data?: any;
-  }
+}
 
 @Component({
     selector: 'app-splitter',
@@ -17,31 +17,31 @@ export class SplitterComponent implements OnDestroy {
     dimensionOptions = new Set(['size', 'minSize', 'maxSize']);
 
     paneContentTemplates: PaneContentTemplate[] = [
-      { name: 'Left Pane' },
-      { name: 'Central Pane' },
-      { name: 'Right Pane' },
-      { name: 'Nested Left Pane' },
-      { name: 'Nested Central Pane' },
-      { name: 'Nested Right Pane' },
-      { name: 'Nested Top Pane' },
-      { name: 'Nested Bottom Pane' },
+        { name: 'Left Pane' },
+        { name: 'Central Pane' },
+        { name: 'Right Pane' },
+        { name: 'Nested Left Pane' },
+        { name: 'Nested Central Pane' },
+        { name: 'Nested Right Pane' },
+        { name: 'Nested Top Pane' },
+        { name: 'Nested Bottom Pane' }
     ];
-  
+
     getPaneState(data: any): string {
-      if (data.resizable !== false && !data.collapsible) {
-        return 'Resizable only';
-      }
-      const resizableText = data.resizable ? 'Resizable' : 'Non-resizable';
-      const collapsibleText = data.collapsible ? 'collapsible' : 'non-collapsible';
-  
-      return `${resizableText} and ${collapsibleText}`;
+        if(data.resizable !== false && !data.collapsible) {
+            return 'Resizable only';
+        }
+        const resizableText = data.resizable ? 'Resizable' : 'Non-resizable';
+        const collapsibleText = data.collapsible ? 'collapsible' : 'non-collapsible';
+
+        return `${resizableText} and ${collapsibleText}`;
     }
-  
+
     filterDimensionOptions(data: any): { key: string; value: any }[] {
-      return Object.entries(data)
-        .reverse()
-        .filter(([key]) => this.dimensionOptions.has(key))
-        .map(([key, value]) => ({ key, value }));
+        return Object.entries(data)
+            .reverse()
+            .filter(([key]) => this.dimensionOptions.has(key))
+            .map(([key, value]) => ({ key, value }));
     }
 
     ngOnDestroy(): void {
