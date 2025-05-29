c4184c501f47c725dd71b2bd41f4076990ec8634
Eldar Iusupzhanov <ferrarijed@gmail.com>
2025-05-29 13:52:12 +0800

    revert theme-builder.service.ts

--- src/app/theme-builder.service.ts
+++ src/app/theme-builder.service.ts
@@ -7,8 +7,7 @@ import { Metadata } from './types/metadata';
 
 @Injectable()
 export class ThemeBuilderService {
-    // private url = 'https://js.devexpress.com/api/themebuilder';
-    private url = 'http://localhost:3000';
+    private url = 'https://js.devexpress.com/api/themebuilder';
 
     constructor(private http: HttpClient) {}
 
